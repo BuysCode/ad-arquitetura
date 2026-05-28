bbca46044bb31d946798fd8b22db4c515239aa3c
Guilherme Buys <guibuys55@gmail.com>
2026-05-28 14:43:42 -0300

    feat: start implementing email lib

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,19 @@
 import { z } from 'zod'
 
+export type Email = `${string}@${string}.${string}`
+export type PhoneNumber = `(${number}) ${number}-${number}`
+
 export const contactFormSchema = z.object({
   name: z.string(),
   phoneNumber: z.string(),
   email: z.email(),
   message: z.string()
 })
+
+export type TSenderData = {
+  email: Email;
+  phoneNumber: PhoneNumber;
+  name: string;
+}
 
 export type ContactFormType = z.infer<typeof contactFormSchema>
